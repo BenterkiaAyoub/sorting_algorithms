f641af45f3c355d8267b7062fae69e371bed4ed9
BenterkiaAyoub <benterkia.ayoub1@gmail.com>
2023-12-18 13:43:59 -0800

    first task

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,9 +1,9 @@
 #include "sort.h"
 
 /**
- * swap_ints - Function to Swap two intgrs in an array.
- * @a: The first int.
- * @b: The second int.
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
 void swap_ints(int *a, int *b)
 {
@@ -15,14 +15,15 @@ void swap_ints(int *a, int *b)
 }
 
 /**
- * bubble_sort - Function that Sort an array of ints.
- * @array: array of ints.
- * @size: size of array.
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
  *
+ * Description: Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t j, length = size;
+	size_t i, len = size;
 	bool bubbly = false;
 
 	if (array == NULL || size < 2)
@@ -31,15 +32,15 @@ void bubble_sort(int *array, size_t size)
 	while (bubbly == false)
 	{
 		bubbly = true;
-		for (j = 0; j < length - 1; j++)
+		for (i = 0; i < len - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				swap_ints(array + j, array + j + 1);
+				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
 				bubbly = false;
 			}
 		}
-		length--;
+		len--;
 	}
 }
--- sort.h
+++ sort.h
@@ -4,6 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,11 +33,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* print */
+/* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Sorting algorithms */
+/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -36,5 +51,4 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
-#endif
-
+#endif /* SORT_H */
