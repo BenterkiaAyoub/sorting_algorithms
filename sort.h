fd73aa05a9196e96b4d2e24ef99ca1f288258fc8
BenterkiaAyoub <benterkia.ayoub1@gmail.com>
2023-12-18 13:50:13 -0800

    first task

--- sort.h
+++ sort.h
@@ -4,21 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Comparison direction macros for bitonic sort */
-#define UP 0
-#define DOWN 1
-
-/**
- * enum bool - Enumeration of Boolean values.
- * @false: Equals 0.
- * @true: Equals 1.
- */
-typedef enum bool
-{
-	false = 0,
-	true
-} bool;
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -33,11 +18,11 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Printing helper functions */
+/* print */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Sorting algoritms */
+/* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -51,4 +36,5 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
-#endif /* SORT_H */
+#endif
+
